43bcc1322add86af27fe83ab0a0d22adb9916b78
robbo-one <73087376+robbo-one@users.noreply.github.com>
2021-02-02 10:45:53 +1300

    importing widgets function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import {getWidgets} from '../api'
 
 function App () {
   const [widgets, setWidgets] = useState([])
 
   useEffect(() => {
-    console.log('using the effect',)
+    // console.log('using the effect',)
+    getWidgets()
+    .then(() => {
+      console.log(getWidgets())
+    })
   }, [])
 
   return (
