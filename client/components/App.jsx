8582f7e5ce72dfd42e13aa16ed016ee6dfd1e50f
robbo-one <73087376+robbo-one@users.noreply.github.com>
2021-02-02 10:36:20 +1300

    widget route working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App () {
+  const [widgets, setWidgets] = useState([])
+
+  useEffect(() => {
+    console.log('using the effect',)
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
