1a6ac5ca31a70e0de367f4ffb5f1aea67ead9705
student <student@eda-bc-02.localdomain>
2021-02-02 15:25:22 +1300

    Show bulleted list of widgets

--- client/components/Widgets.jsx
+++ client/components/Widgets.jsx
@@ -9,15 +9,24 @@ const Widgets = () => {
       console.log('Effects working')
       getWidgets().then(widgets => {
         setWidgets(widgets) //set to be widget object returned
-      })
-     // return getWidgets()
-     // .then(console.log(getWidgets))
-    }, [])
-    return(widgets.map => {
-    <p>Mounted</p>)
-}
-
-  
- 
+      })}, [])
+      return (
+        <>
+            {widgets.map(aWidget => (
+               <ul key={aWidget.id}>
+                <li>{aWidget.name}</li>
+                <li>{aWidget.price}</li>
+                <li>{aWidget.mfg}</li>
+                <li>{aWidget.inStock}</li>
+              </ul>
+            ))}
+        </>
+  )}
+            
 
 export default Widgets
+
+
+{/* <p>Mounted</p> */}
+   {/* // return getWidgets()
+     // .then(console.log(getWidgets)) */}
