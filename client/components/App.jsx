bd88f6f1feb035ba6f32df3d7b70d94b7c1b44bb
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-02-02 15:26:21 +1300

    Delete commented code from App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Widget from "./Widget";
 
-// import { getWidgets } from '../api'
-
-
 function App () {
-
-// const [widgets, setWidgets] = useState([])
-
-// useEffect(() => {
-//   getWidgets()
-//     .then(widgets => {
-//       setWidgets(widgets)
-//     })
-//   }, []
-// )
   
- 
-  // return (
-  //   <div>
-  //     <h1>Widgets for the win!</h1>
-  //     <ul>{widgets.map(widget => (
-  //       <li key={widget.id}>
-  //         <div>Name: {widget.name}</div>
-  //         <div>In stock: {widget.inStock}</div>
-  //         <div>MFG: {widget.mfg}</div>
-  //       </li>
-  //     ))}</ul>
-  //   </div>
-  // )
-
   return (
     <Widget />
   )
